ea2246d7970e4de04601a650e2f80bf333c5c068
Kazuki Hamasaki <kazuki-h@iij.ad.jp>
2022-09-04 23:34:37 +0900

    add create set list script

---
+++ lib/createSetList.ts
@@ -0,0 +1,46 @@
+const { load } = require('js-yaml')
+const { readFileSync } = require('fs')
+
+type Musics = {
+    videoId: string,
+    videoTitle: string,
+    start: string,
+    end: string,
+    meta: {
+        ja: {
+            title: string,
+            artist: string
+        }
+    }
+}
+
+const loadYaml = <T = ReturnType<typeof load>>(...args: Parameters<typeof load>): T => load(...args);
+
+const loadMusics = () => {
+    return loadYaml<Musics[]>(readFileSync('./src/data/musics.yaml', 'utf8'));
+}
+
+const createSetList = () => {
+    const musics = loadMusics();
+
+    let currentVideoId = '';
+    let listIndex = 0;
+
+    musics.forEach((music) => {
+        if (currentVideoId !== music.videoId) {
+            // next video
+            currentVideoId = music.videoId;
+            listIndex = 1;
+
+            console.log('');
+            console.log(music.videoTitle);
+            console.log('■ セトリ / Set List');
+        }
+
+        console.log(`${String(listIndex).padStart(2, '0')}. ${music.start} ${music.meta.ja.title} / `);
+
+        listIndex++;
+    });
+}
+
+createSetList();
