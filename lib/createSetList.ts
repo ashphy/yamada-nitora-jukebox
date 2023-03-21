8e6428ccf10e58b691b3071fb85096fa8b456fdf
Kazuki Hamasaki <kazuki-h@iij.ad.jp>
2023-03-21 22:08:28 +0900

    fix set list script for changing yaml structure

--- lib/createSetList.ts
+++ lib/createSetList.ts
@@ -2,7 +2,7 @@ const { load } = require('js-yaml')
 const { readFileSync } = require('fs')
 
 type Musics = {
-    videoId: string,
+    video: string,
     videoTitle: string,
     start: string,
     end: string,
@@ -14,26 +14,35 @@ type Musics = {
     }
 }
 
+type Videos = {
+    videoId: string,
+    videoTitle: string,
+    date: string
+}
+
 const loadYaml = <T = ReturnType<typeof load>>(...args: Parameters<typeof load>): T => load(...args);
 
-const loadMusics = () => {
-    return loadYaml<Musics[]>(readFileSync('./src/data/music.yaml', 'utf8'));
+const loadYamlFromFile = <T>(fileName: string) => {
+    return loadYaml<T[]>(readFileSync(fileName, 'utf8'));
 }
 
 const createSetList = () => {
-    const musics = loadMusics();
+    const musics = loadYamlFromFile<Musics>('./src/data/music.yaml');
+    const videos = loadYamlFromFile<Videos>('./src/data/video.yaml');
 
     let currentVideoId = '';
     let listIndex = 0;
 
     musics.forEach((music) => {
-        if (currentVideoId !== music.videoId) {
+        if (currentVideoId !== music.video) {
             // next video
-            currentVideoId = music.videoId;
+            currentVideoId = music.video;
             listIndex = 1;
 
+            const video = videos.find((video) => video.videoId === currentVideoId)
+
             console.log('');
-            console.log(music.videoTitle);
+            console.log(video?.videoTitle);
             console.log('■ セトリ / Set List');
         }
 
